13248f2a9d26832599534e0693da6823eac78ffc
Philipp J. Haus <109821624+IcedCoffeeDrinker@users.noreply.github.com>
2024-12-31 18:22:50 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -348,11 +348,7 @@ const CONFIG = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
-      target="_blank"
-      rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+  footer: `Thank you for visiting my profile!`,
 
   enablePWA: true,
 };
