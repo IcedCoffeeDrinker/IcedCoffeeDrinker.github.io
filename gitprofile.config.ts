34d37a3783269ab7fe6ccf8676607b745aa99fe4
Philipp J. Haus <109821624+IcedCoffeeDrinker@users.noreply.github.com>
2024-12-31 16:07:53 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -38,15 +38,15 @@ const CONFIG = {
             'After three semesters of daily work in the workshop, me and my team went to race our selfbuild solar car at the Texas Motor Speedway against teams from all over the US.',
           imageUrl:
             'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/solar_car_early_stage.jpeg',
-          link: 'https://example.com',
+          link: 'https://www.solarcarchallenge.org/challenge/',
         },
         {
           title: 'Kent AI LAB (K.A.I.L)',
           description:
             "As co-leader of our school's AI club I organized interactive events for our 56 members, held presentations at other schools, and took part in public discussions with experts, so eg. Krystyn Gutu.",
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/',
+          link: '',
         },
         {
           title: 'Robotic Arm',
@@ -54,39 +54,39 @@ const CONFIG = {
             'At around age 13 I got interested in robotics. This 3D printed robotic arm was my first take on the topic and served as the test ground for a multiple of subsequent projects.',
           imageUrl:
             'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/robotic_arm.jpeg',
-          link: 'https://example.com',
+          link: '',
         },
         {
           title: 'MagicMirror2',
           description:
             'To stay organized, my 12 year old me decided to build the MagicMirror2 open source project, further modifying the code to create productive digital art.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/MagicMirror2.png',
+          link: 'https://magicmirror.builders/',
         },
         {
           title: 'Kent: Digital Electronics',
           description:
             'In my year abroad at Kent School I was faced with the opportunity to take engineering classes. In my two semesters of "Digital Electronics", and one semester "Aerospace Engineering" I build an extensive portfolio.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/digital_electronics_1.png',
+          link: 'https://sites.google.com/view/philipp-engineering/start',
         },
         {
           title: 'Pwnagotchi',
           description:
             'The Pwnagotchi open source project introduced me to the world of cybersecurity. After assembling and setting up the device, I was faced with a multiple of software and hardware issues, which I managed to overcome afte many hours of debugging.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/pwnagotchi_final.png',
+          link: '',
         },
         {
           title: 'CAD Project: Buzzer',
           description:
             'As a result of my interest in product design, I develoed the "Sound Buzzer". This poject tried to combine the intended function with aesthetic design.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/CAD_buzzer.png',
+          link: '',
         },
         {
           title: 'Gameboy Emulator + Display Driver',
@@ -94,31 +94,31 @@ const CONFIG = {
             'Initially intended as a birthday gift for my little brother, my Gameboy Emulator Project cost me well over 50 hours of debugging. I ended up creating my own driver.',
           imageUrl:
             'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/gameboy_prototyping.jpeg',
-          link: 'https://example.com',
+          link: '',
         },
         {
           title: '3D Simulations & Animations',
           description:
             'Due to my interest in Digital Effects I explored learned to create 3D environments in Blender and particularly enjoyed playing with the physics engine.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/fluid_simulation.png',
+          link: 'https://www.youtube.com/@dreamerunderpinksky2691',
         },
         {
           title: '3D Projection',
           description:
             'Fascinated by the mathematics of 3D projection I decided to build a render engine that displays 3D model files (.obj, .stl) as wireframes. I later taught the mathematical concepts of ortographic and perspective projection to my peers.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/render_wireframe.png',
+          link: '',
         },
         {
           title: 'Generative Art & Perlin Noise',
           description:
             'Inspired by aesthetic NFT art I created various generative art, often making use of fractal-like structures, beautiful colors, and perlin-noise. I was particularly interested in flow maps.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/mandelbulb.png',
+          link: 'hhttps://github.com/IcedCoffeeDrinker/da_good_stuff',
         },
         {
           title: 'Local Diffusion Models',
@@ -126,39 +126,39 @@ const CONFIG = {
             'To harness the full power of image generation models I decided to run various diffusion models locally. This allowed me to modify the algorythms and make use of experimental state-of-the-art modifications',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: '',
         },
         {
           title: 'AI Article Bot (SEO)',
           description:
             'Meant to earn me some extra pocket money, this project creates a conversation between up to ten AI agents debating about topic, content, and images. Hundreds of artilces can be generated in an hour.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/article_bot_logfile.png',
+          link: '',
         },
         {
           title: 'Personalized Local AI Assistant',
           description:
             'High subsription fees for major AI agents led me to build a free local system, tuned to my preferences. My assistant has multiple personalities to choose from and chat-independent memory.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/AI_assistant.png',
+          link: '',
         },
         {
           title: 'First AI Project: Guessing Sequences',
           description:
             'When I was 10 years old I learned how to code a sequence guessing AI through an Indian tech-tutorial. This taught me the basics of neurons and statistical predictions.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/first_AI_project.png',
+          link: '',
         },
         {
           title: 'Ways to Calculate PI',
           description:
             'Fascinated by the random occurences of pi in the mathematical world I build various programs to estimate pi in interesting ways.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/guessing_pi.png',
+          link: '',
         },
         {
           title: 'Classmate Name Trainer',
@@ -166,15 +166,15 @@ const CONFIG = {
             "To get down all the names of my peers during my year abroad I built a programm that would quiz me on a person's name based on a picture that it scraped from the school's website.",
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: '',
         },
         {
           title: 'Scratch 3D Map',
           description:
             'Feeling limited by the 2D canvas of scratch, I decided to build a 3D maze game as a school project. My IT teacher was a little supriesed.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/scratch_raycasting.png',
+          link: 'https://scratch.mit.edu/projects/568312991/',
         },
         {
           title: 'Coding a Restaurant Homepage',
@@ -182,7 +182,7 @@ const CONFIG = {
             'My first ever IT class project was to build a website for a local business. I setelt for my favorite asian restaurant, motivated to spice their website up as much as possilbe.',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: '',
         },
         {
           title: 'Minecraft Binary Logic',
@@ -190,23 +190,23 @@ const CONFIG = {
             'At around age 9 I developed a fascination for binary logic. I replicated full- and half adders, encoders, decoders, memory units, and displays in Minecraft.',
           imageUrl:
             'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+          link: '',
         },
         {
           title: 'Mining Crypto on Raspberry Pi',
           description:
             'As a joke I mined Monero on my Raspberry Pi in 7th grade. My parents said they would pay the electricity bill for three months, allowing me to dive deeper into the topic.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/monero_mining.png',
+          link: '',
         },
         {
           title: 'Linux & MacOS Modifications',
           description:
             'As a huge fan of aesthetic design and productivity modifications, I heavily modded my Mac and Linux devices. All my devices are now very fancy looking, though their productivity is questionable.',
           imageUrl:
-            'https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg',
-          link: 'https://example.com',
+            'https://raw.githubusercontent.com/IcedCoffeeDrinker/IcedCoffeeDrinker.github.io/main/images/mac_ricing.png',
+          link: '',
         },
       ],
     },
